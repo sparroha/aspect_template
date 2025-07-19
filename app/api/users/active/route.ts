6c9da199a8536ad2d61b06118816bb9af55a982c
Keith <940398+sparroha@users.noreply.github.com>
2025-07-19 14:44:42 -0400

    .
    months went by before submitting this. not sure what I was doing but it looks legit

---
+++ app/api/users/active/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import sql from "../../../../lib/,base/sql";
+import { RegistryEntry } from "../../registry/route";
+export const dynamic = 'force-dynamic'
+export const fetchCache = 'force-no-store'
+
+export type ActiveUser = {
+    name: string,
+    access: number,
+    time: number
+}
+
+export async function GET(req, res): Promise<NextResponse<{data: ActiveUser[]}>>{
+    const ACTIVEUSERS = 'active_users'
+    //const nonuser: ActiveUser = {name: '420: No Users Active', access: 2, time: Date.now()}
+    //let regdata: ActiveUser[] = []
+    const [users]: [RegistryEntry] = await sql`SELECT * FROM aspect_registry_ WHERE name = ${ACTIVEUSERS};`
+    const usersData: ActiveUser[] = users?JSON.parse(users.registry_data):[]
+    if(!users) return NextResponse.json({data: []})
+    
+    return NextResponse.json({data: usersData})
+}
